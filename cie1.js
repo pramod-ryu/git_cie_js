3a81e2ff8dc2de2418650da86611135917b58fc6
gowthampoojar <gowthampoojary4210@gmal.com>
2024-07-26 11:49:51 +0530

    added line

--- cie1.js
+++ cie1.js
@@ -10,6 +10,7 @@ function toggle(i)
             {
                 case "co1":name="Pramod";break;
                 case "co2":name="Anvitha";break;
+                case "22013":name="Gowtham";break;
                 default:name="";
             }
         if (name_element_visibility==="hidden")
