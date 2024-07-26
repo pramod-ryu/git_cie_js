bcaf1df1dfcf0ea674c2b736dd1568db8269feb8
varshitha <varshavarshitha118@gmail.com>
2024-07-26 12:26:27 +0530

    change by varshitha

--- cie1.js
+++ cie1.js
@@ -10,6 +10,7 @@ function toggle(i)
             {
                 case "co1":name="Pramod";break;
                 case "co2":name="Anvitha";break;
+                case "22054":name="Varshitha";break;
                 default:name="";
             }
         if (name_element_visibility==="hidden")
