2d851617e9bc16b6e97b731e5d8860c7fcb0cdc1
Supreetha <supreethanaik92@gmail.com>
2024-07-26 12:38:05 +0530

    added

--- cie1.js
+++ cie1.js
@@ -10,6 +10,7 @@ function toggle(i)
             {
                 case "co1":name="Pramod";break;
                 case "co2":name="Anvitha";break;
+                case "22048":name="Supreetha";break;
                 default:name="";
             }
         if (name_element_visibility==="hidden")
